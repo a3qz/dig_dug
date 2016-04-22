206c1a854e5f43fee6650b32536c8af929d86f01
Ryan Steven Michalec <rmichale@fitz87.helios.nd.edu>
2016-04-21 20:54:06 -0400

    dsa;lkjfsakjdhflkjwahsdwlkfhlkjdsafhlkjdsakjf

--- ryan_inclass/background/main.cpp
+++ ryan_inclass/background/main.cpp
@@ -1,16 +1,35 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2015)
 and may not be redistributed without written permission.*/
 
+/////////////////////////////////////////// PLAN FOR THE FUTURE: NO ARRAY< JUST DELETE BEHIND MAN EVERY TIME
+
+
 //Using SDL, SDL_image, standard math, and strings
+#include <ctime>
+#include <iostream>
 #include <SDL2/SDL.h>
 #include <unistd.h>
 #include <SDL2/SDL_image.h>    // this actually gets SDL2_image
 #include <stdio.h>
 #include <string>
+using namespace std;
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 504;
 const int SCREEN_HEIGHT = 684;
+//Starts up SDL and creates window
+bool init();
+
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
 
 
 //Texture wrapper class
@@ -45,6 +64,60 @@ class LTexture
 		int mHeight;
 };
 
+class Point{
+	friend class tempBoard;
+
+	public:
+		Point(  );
+		Point( int, int, char );
+		~Point();
+		void setState(int);
+		int getState();
+		int getx();
+		int gety();
+		char getColor();
+		void setColor();
+		void setx();
+	private:
+		char color;
+		int state;
+		int x;
+		int y;
+
+
+};
+
+Point::Point(){
+	state = 1;
+}
+Point::~Point(){}
+Point::Point(int newx , int newy, char col){
+	x = newx;
+	y = newy;
+	color = col;
+	state = 1;
+	
+}
+
+void Point::setState(int g){
+	state = g;
+}
+
+int Point::getState(){
+
+	return state;
+}
+int Point::getx(){
+	return x;
+}
+int Point::gety(){
+	return y;
+}
+char Point::getColor(){
+	return color;
+}
+
+
 class tempBoard{
 
 
@@ -56,6 +129,8 @@ class tempBoard{
 	private:
 		//Scene sprites
 		SDL_Rect gSpriteClips;
+		Point points[504][684];
+
 		LTexture gSpriteSheetTexture;
 
 
@@ -94,7 +169,30 @@ Player::Player()
 
 }
 
-tempBoard::tempBoard(){}
+tempBoard::tempBoard(){
+	for ( int j = 0; j < 684; j++){
+		for( int i = 0; i < 504; i ++){
+			points[i][j].x = i;
+			points[i][j].y = j;
+			if ( j < 108){
+				points[i][j].color = 'u';
+			}
+			else if (j < 252){
+				points[i][j].color = 'y';
+			}
+			else if (j < 369){
+				points[i][j].color = 'o';
+			}
+			else if (j < 540){
+				points[i][j].color = 'r';
+			}
+			else{
+				points[i][j].color = 'b';
+			}
+		
+		}
+	}
+}
 
 tempBoard::~tempBoard(){
 
@@ -175,26 +273,45 @@ void Player::move(char dir)
 
 void tempBoard::render()
 {
-	gSpriteSheetTexture.render( 0, 0, &gSpriteClips);
+	for ( int j = 0; j < 684; j++){
+		for( int i = 0; i < 504; i ++){
+			if( points[i][j].state ==  1){
+				switch(points[i][j].color){
+					case 'u':
+						SDL_SetRenderDrawColor(gRenderer, 0, 0, 225, 0);
+						break;
+					case 'o':
+						SDL_SetRenderDrawColor(gRenderer, 255, 128, 0, 0);
+						break;
+					case 'r':
+						SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 0);
+						break;
+					case 'y':
+						SDL_SetRenderDrawColor(gRenderer, 255, 255, 0, 0);
+						break;
+					case 'b':
+						SDL_SetRenderDrawColor(gRenderer, 102, 51, 0, 0);
+						break;
+
+
+				
+				
+				}
+			
+			}
+			else{
+				SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+			}
+			SDL_RenderDrawPoint(gRenderer, i, j);
+		}
+	}
+	//gSpriteSheetTexture.render( 0, 0, &gSpriteClips);
 }
 
 void Player::render()
 {
 	gSpriteSheetTexture.render( xpos, ypos, &gSpriteClips[ (dirstat + walkanimatecount%2) ] );
 }
-//Starts up SDL and creates window
-bool init();
-
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
 
 
 LTexture::LTexture()
@@ -319,7 +436,7 @@ bool init()
 		else
 		{
 			//Create renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 			if( gRenderer == NULL )
 			{
 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
@@ -477,7 +594,8 @@ void close()
 
 int main( int argc, char* args[] )
 {
-  tempBoard board;
+	int lastmod = 0;
+	tempBoard board;
 	Player player_character;	
 	//Start up SDL and create window
 	if( !init() )
@@ -503,6 +621,8 @@ int main( int argc, char* args[] )
 			SDL_Event e;
 
 			//While application is running
+			SDL_RenderClear( gRenderer );
+			board.render();
 			while( !quit )
 			{
 				//Handle events on queue
@@ -513,37 +633,50 @@ int main( int argc, char* args[] )
 					{
 						quit = true;
 					}
-					else if (e.type == SDL_KEYDOWN)
+					else if ((e.type == SDL_KEYDOWN) & ((time(NULL) - lastmod) > 1))
 					{
+						SDL_RenderClear( gRenderer );
+						board.render();
 						switch( e.key.keysym.sym )
 						{
 
 							case SDLK_UP:
 								player_character.move('u');
+								lastmod = time(NULL);
+				//	player_character.render();
+
 								break;
 							case SDLK_DOWN:
 								player_character.move('d');
+								lastmod = time(NULL);
+					//player_character.render();
 								break;
 							case SDLK_LEFT:
 								player_character.move('l');
+								lastmod = time(NULL);
+					//player_character.render();
 								break;
 							case SDLK_RIGHT:
 								player_character.move('r');
+								lastmod = time(NULL);
+					//player_character.render();
+								cout << "MOVED" << endl;
+								break;
+							case SDLK_z:
+								board.render();
 								break;
 
 						}
+					player_character.render();
+					SDL_RenderPresent( gRenderer );
 					}
 				}
 
 				//Clear screen
 				//SDL_SetRenderDrawColor( gRenderer, 255, 156, 0, 0xFF );
-				SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 0xFF );
-				SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 0xFF );
-				SDL_RenderClear( gRenderer );
+				//SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 0xFF );
 
 				//Render top left sprite
-        board.render();
-				player_character.render();
         //usleep(5000);
 
 				//Render top right sprite

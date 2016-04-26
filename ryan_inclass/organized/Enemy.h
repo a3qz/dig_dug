5afaed492ae3cc00db4527b294565222107d161a
jesscioffi <jcioffi@nd.edu>
2016-04-25 22:26:58 -0400

    Enemy class

--- ryan_inclass/organized/Enemy.h
+++ ryan_inclass/organized/Enemy.h
@@ -9,16 +9,31 @@
 #include "tempBoard.h"
 #include "Character.h"
 
-
+using namespace std;
 
 class Enemy : public Character{
 
 	friend class tempBoard;
+	
+	public:
+		void draw(int); // draws int number of enemies on the board
+		void AI(); // enemy's movement
+		
+		/*
+		Get Functions
+		*/
+		int getXpos();
+		int getYpos();
+		int getState();
+		int getSpeed();
+		int getNum();
 
-
-
-
-
+	private:
+		int xpos; // x position
+		int ypos; // y position
+		int speed; // how fast the enemy is travelling - incremented as level increases
+		int state; // moving, attacking, dead
+		int num; // number of enemies
 
 };
 
